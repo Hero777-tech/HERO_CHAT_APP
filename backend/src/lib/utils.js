a4a0fa1ed5345ceeb6834f8bfbe717d26d50b049
Aditya Nath <pro.codenow@gmail.com>
2025-10-28 01:49:05 +0530

    working

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,7 +15,8 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     sameSite: "Lax", // CSRF attacks
-    secure: true,
+    // secure: true,
+    path: "/",
   });
 
   return token;
